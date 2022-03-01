75d43ae944af5af18721296c1477e6b3fa20975c
Chirag Mittal <chirag.workid@gmail.com>
2022-03-01 06:46:34 +0530

    connected backend part2

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 import './cart.dart';
 
@@ -23,16 +26,181 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> fetchAndSetOrders() async {
+    var url = Uri.https(
+      'shop-app-d0404-default-rtdb.firebaseio.com',
+      '/orders.json',
+    );
+
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+
+    if (extractedData == null) {
+      return;
+    }
+
+
+    extractedData.forEach(
+      (orderId, orderData) {
+        loadedOrders.add(
+          OrderItem(
+            id: orderId,
+            amount: orderData['amount'],
+            dateTime: DateTime.parse(orderData['dateTime']),
+            products: (orderData['products'] as List<dynamic>).map(
+              // (item) {
+              //   CartItem(
+              //     id: item['id'],
+              //     title: item['title'],
+              //     quantity: item['quantity'],
+              //     price: item['price'],
+              //   );
+              // },
+              (item) => CartItem(
+                      id: item['id'],
+                      price: item['price'],
+                      quantity: item['quantity'],
+                      title: item['title'],
+                    ),
+            ).toList(),
+          ),
+        );
+      },
+    );
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+  }
+
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final timeStamp = DateTime.now();
+
+    var url = Uri.https(
+      'shop-app-d0404-default-rtdb.firebaseio.com',
+      '/orders.json',
+    );
+
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          'amount': total,
+          'dateTime': timeStamp.toIso8601String(),
+          'products': cartProducts
+              .map(
+                (cartP) => {
+                  'id': cartP.id,
+                  'title': cartP.title,
+                  'quantity': cartP.quantity,
+                  'price': cartP.price,
+                },
+              )
+              .toList(),
+        },
+      ),
+    );
+
     _orders.insert(
       0,
       OrderItem(
-        id: DateTime.now().toString(),
+        id: json.decode(response.body)['name'],
         amount: total,
-        dateTime: DateTime.now(),
+        dateTime: timeStamp,
         products: cartProducts,
       ),
     );
     notifyListeners();
   }
 }
+
+
+// import 'dart:convert';
+
+// import 'package:flutter/foundation.dart';
+// import 'package:http/http.dart' as http;
+
+// import './cart.dart';
+
+// class OrderItem {
+//   final String id;
+//   final double amount;
+//   final List<CartItem> products;
+//   final DateTime dateTime;
+
+//   OrderItem({
+//     @required this.id,
+//     @required this.amount,
+//     @required this.products,
+//     @required this.dateTime,
+//   });
+// }
+
+// class Orders with ChangeNotifier {
+//   List<OrderItem> _orders = [];
+
+//   List<OrderItem> get orders {
+//     return [..._orders];
+//   }
+
+//   Future<void> fetchAndSetOrders() async {
+//     final url = Uri.https('shop-app-d0404-default-rtdb.firebaseio.com', '/orders.json');
+//     final response = await http.get(url);
+//     final List<OrderItem> loadedOrders = [];
+//     final extractedData = json.decode(response.body) as Map<String, dynamic>;
+//     if (extractedData == null) {
+//       return;
+//     }
+//     extractedData.forEach((orderId, orderData) {
+//       loadedOrders.add(
+//         OrderItem(
+//           id: orderId,
+//           amount: orderData['amount'],
+//           dateTime: DateTime.parse(orderData['dateTime']),
+//           products: (orderData['products'] as List<dynamic>)
+//               .map(
+//                 (item) => CartItem(
+//                       id: item['id'],
+//                       price: item['price'],
+//                       quantity: item['quantity'],
+//                       title: item['title'],
+//                     ),
+//               )
+//               .toList(),
+//         ),
+//       );
+//     });
+//     _orders = loadedOrders.reversed.toList();
+//     notifyListeners();
+//   }
+
+//   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+//     final url = Uri.https('shop-app-d0404-default-rtdb.firebaseio.com', '/orders.json');
+//     final timestamp = DateTime.now();
+//     final response = await http.post(
+//       url,
+//       body: json.encode({
+//         'amount': total,
+//         'dateTime': timestamp.toIso8601String(),
+//         'products': cartProducts
+//             .map((cp) => {
+//                   'id': cp.id,
+//                   'title': cp.title,
+//                   'quantity': cp.quantity,
+//                   'price': cp.price,
+//                 })
+//             .toList(),
+//       }),
+//     );
+//     _orders.insert(
+//       0,
+//       OrderItem(
+//         id: json.decode(response.body)['name'],
+//         amount: total,
+//         dateTime: timestamp,
+//         products: cartProducts,
+//       ),
+//     );
+//     notifyListeners();
+//   }
+// }
